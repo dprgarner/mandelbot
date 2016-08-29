a18129af7a10fabcffa353ccd4e1066011eefc49
David Garner <dprgarner@gmail.com>
2016-08-29 19:29:32 +0100

    Reordering and comments

--- viewer.elm
+++ viewer.elm
@@ -1,20 +1,30 @@
 import Html exposing (Attribute, div, text, Html)
-import Html.Attributes exposing (style)
 import Html.App exposing (program)
-import Html.Events exposing (on, onClick)
+import Html.Attributes exposing (style)
+import Html.Events exposing (on)
 import Json.Decode exposing ((:=), Decoder, int, map, object2)
 
 type alias Model = {coords : (Int, Int)}
 type Msg = Click (Int, Int)
 
-main = program {
-  init = init,
-  view = view,
-  update = update,
-  subscriptions = subscriptions
-}
+--
+-- Setup
+--
 
+main =
+  program {
+    init = init,
+    view = view,
+    update = update,
+    subscriptions = subscriptions
+  }
+
+init : (Model, Cmd Msg)
 init = ({coords = (0, 0)}, Cmd.none)
+
+--
+-- View
+--
 
 decodeOffset : Decoder (Int, Int)
 decodeOffset =
@@ -45,11 +55,19 @@ view model =
       ]] [ text "x"]
     ]
 
+--
+-- Update
+--
+
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Click coords ->
       ({model | coords = coords}, Cmd.none)
+
+--
+-- Subscriptions
+--
 
 subscriptions : Model -> Sub Msg
 subscriptions _ = Sub.none
