9b949110fe69544340c53906cd16e463d508060e
David Garner <dprgarner@gmail.com>
2017-02-04 18:21:22 +0000

    Scry for a mandelbrot copy

--- find.js
+++ find.js
@@ -10,9 +10,7 @@ const constructSet = require('./mandelbrot').constructSet;
 const renderSetToFile = require('./mandelbrot').renderSetToFile;
 
 function collateDepths(set, {width, height, depth}) {
-  let startTime = Date.now();
-  // console.log(`Collating depths...`);
-  let depthsCollation = {[null]: [], orphans: []};
+  let depthsCollation = {[null]: []};
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
       let depth = set[y][x];
@@ -24,7 +22,6 @@ function collateDepths(set, {width, height, depth}) {
   for (let i = 0; i < depth; i++) {
     if (depthsCollation[i]) depthsCollation.maxDepth = i;
   }
-  // console.log(`Collated depths after ${Date.now() - startTime}ms`);
   return depthsCollation;
 }
 
@@ -35,7 +32,7 @@ function toComplexCoords({pixelX, pixelY}, {x, y, width, height, scale}) {
   return {x: newX, y: newY}; 
 }
 
-function getInterestingPixelFromDepths(
+function getRandomNearMandelbrotPoint(
   depthsCollation, {x, y, width, height, depth, scale}, threshold
 ) {
   let tally = 0;
@@ -59,7 +56,7 @@ function getInterestingPixelFromDepths(
   return toComplexCoords({pixelX, pixelY}, {x, y, width, height, scale});
 }
 
-function getInterestingMandelbrotPoint(
+function getRandomMandelbrotPoint(
   depthsCollation, {x, y, width, height, depth, scale}
 ) {
   let {x: pixelX, y: pixelY} = depthsCollation[null][
@@ -79,100 +76,271 @@ function render(set, params, fileName) {
   });
 }
 
-function findInterestingPoint({width, height}) {
+// Split the set into 10x10-pixel segments, count the proportion of points
+// which are in the Mandelbrot set, and return these proportions as a matrix.
+function getPercentProfile(set) {
+  const xSize = 10;
+  const ySize = 10;
+  const yRange = Math.floor(set.length / ySize);
+  const xRange = Math.floor(set[0].length / xSize);
+  let profile = [];
+
+  for (let y = 0; y < yRange; y++) {
+    profile[y] = [];
+    for (let x = 0; x < xRange; x++) {
+      let mandelbrotCount = 0;
+      for (let j = 0; j < ySize; j++) {
+        for (let i = 0; i < xSize; i++) {
+          if (set[ySize * y + j][xSize * x + i] === null) {
+            mandelbrotCount++;
+          }
+        }
+      }
+      profile[y][x] = mandelbrotCount / (xSize * ySize);
+    }
+  }
+  return profile;
+}
+
+// A reduce function for the profile set.
+function reduceProfile(profile, init, fn) {
+  const yRange = profile.length;
+  const xRange = profile[0].length;
+
+  let acc = init;
+  for (let j = 0; j < yRange; j++) {
+    for (let i = 0; i < xRange; i++) {
+      acc = fn(acc, profile[j][i]);
+    }
+  }
+  return acc;
+}
+
+const getMaxProportion = (profile) => reduceProfile(profile, 0, Math.max);
+
+function getMaxProportionArea(profile) {
+  let max = getMaxProportion(profile);
+
+  const yRange = profile.length;
+  const xRange = profile[0].length;
+
+  // Find the areas which have the highest proportion of Mandelbrot points.
+  let areas = [];
+  for (let j = 0; j < yRange; j++) {
+    for (let i = 0; i < xRange; i++) {
+      if (profile[j][i] === max) {
+        areas.push([j, i]);
+      }
+    }
+  }
+
+  // When there are multiple maximal areas, we find and return the one with the
+  // greatest sum of its neighbours.
+  function sumNeighbours([j, i]) {
+    sum = 0;
+    if (j > 0) sum += profile[j-1][i];
+    if (j < yRange - 1) sum += profile[j+1][i];
+    if (i > 0) sum += profile[j][i-1];
+    if (i < xRange - 1) sum += profile[j][i+1];
+    return sum;
+  }
+
+  let maxArea;
+  if (areas.length === 1) {
+    maxArea = areas[0];
+  } else {
+    let neighbourAreas = areas.map(sumNeighbours);
+    maxArea = areas[neighbourAreas.indexOf(Math.max(...neighbourAreas))];
+  }
+
+  return {y: (maxArea[0] + 0.5) / yRange, x: (maxArea[1] + 0.5) / xRange};
+}
+
+function lotsOfGrainyAreas(profile) {
+  let proportion = reduceProfile(profile, 0, (acc, p) => 
+    acc += (p > 0 && p < 0.5) ? 1 : 0
+  );
+  return proportion / (profile.length * profile[0].length) > 0.25;
+}
+
+function lotsOfNotMandlebrotAreas(profile) {
+  let proportion = reduceProfile(profile, 0, (acc, p) => 
+    acc += (p === 0) ? 1 : 0
+  );
+  return proportion / (profile.length * profile[0].length) > 0.25;
+}
+
+const totalProportion = (profile) => (
+  reduceProfile(profile, 0, (acc, p) => acc + p) / (profile.length * profile[0].length)
+);
+
+// Find which sides of the image have a non-trivial proportion of Mandelbrot
+// set points.
+function getMandelbrotSides(set) {
+  const yRange = set.length;
+  const xRange = set[0].length;
+
+  let sides = {
+    N: set[0],
+    S: set[yRange - 1],
+    E: set.map(arr => arr[xRange - 1]),
+    W: set.map(arr => arr[0]),
+  }
+
+  return _.chain(sides)
+  .pick((side) => side.filter(p => p === null).length / side.length > 0.1)
+  .keys()
+  .value();
+}
+
+
+// For logging.
+let attempt=-1;
+function scry({width, height}) {
   let depthAdjust = 500;
 
   let x = -0.5;
   let y = 0;
-  let level = 0;
-  let depth = depthAdjust + 100 * level;
 
-  let redraws = 0;
-  let targetLevel = 20;
-  let done = false;
+  const targetLevel = 16;
+  let level;
+  let depth;
+  let scale;
 
-  for (let i = 0; i < 20; i++) {
+  // Initial scry: get to a reasonably deep spot.
+  for (level = 0; level < targetLevel; level += 4) {
+    attempt++; // Logging
     depth = depthAdjust + 100 * level;
-    let params = {
-      scale: Math.pow(2, -6 - level), // (size in complex coords) / (size in pixels)
-      x,
-      y, 
-      depth,
-      width,
-      height,
-      level,
-    };
+    scale = Math.pow(2, -6 - level); // (size in complex coords) / (size in pixels)
+    let params = {scale, x, y, depth, width, height, level};
 
-    console.log(`
-    ${i}: Level: ${level}, Depth: ${depth}`);
-
-    let startTime = Date.now();
     let set = constructSet(params);
-
-    render(set, params, `./frames/${i}_${level}_${depth}_${x}_${y}.gif`);
+    // Logging
+    console.log(`    ${attempt}: Level: ${level}, Depth: ${depth}`);
+    render(set, params, `./frames/${attempt}_${level}_${depth}_${x}_${y}.gif`);
 
     let depthsCollation = collateDepths(set, {width, height, depth});
     let threshold = Math.floor(
       (0.1 * Math.random()) * (width * height - depthsCollation[null].length)
     );
-    if (depthsCollation.orphans.length) {
-      console.log(depthsCollation.orphans);
-      console.log(`./frames/${i}_${level}_${depth}_${x}_${y}.gif`);
-      break;
+
+    if (depthsCollation.maxDepth < depth / 2) {
+      console.log('Depth unnecessarily high: adjusting...') 
+      depthAdjust = Math.round(depthAdjust / 2);
     }
 
-    if (depthsCollation.maxDepth < depth / 10 && redraws < 3) {
-      if (!depthsCollation[null].length) {
-        console.log('Nothing interesting here: zooming out');
-        level = Math.max(level - 4, 0);
-      } else {
-        console.log('Depth unnecessarily high: adjusting...') 
-        depthAdjust = Math.round(depthAdjust / 2);
-      }
-      redraws++;
+    let deepPixel = getRandomNearMandelbrotPoint(depthsCollation, params, threshold);
+    x = deepPixel.x;
+    y = deepPixel.y;
+  }
+
+  // Final traversal: try and hone in on a Mandelbrot point.
+  // Five tries.
+  let tries = 5;
+  let potentials = [];
+
+  for (let j = 0; j < tries; j++) {
+    attempt++; // Logging
+    depth = depthAdjust + 100 * level;
+    scale = Math.pow(2, -6 - level); // (size in complex coords) / (size in pixels)
+    let params = {scale, x, y, depth, width, height, level};
+
+    let set = constructSet(params);
+    // Logging
+    console.log(`      ${attempt}: Level: ${level}, Depth: ${depth}`);
+    render(set, params, `./frames/${attempt}_${level}_${depth}_${x}_${y}.gif`);
+
+    let profile = getPercentProfile(set);
+    // console.log(profile);
+    let maxProportion = getMaxProportion(profile);
+
+    // Stop if there are no likely candidates for a Mandelbrot copy
+    if (getMaxProportion(profile) === 0) {
+      console.log('Nothing here');
+      level--;
       continue;
     }
 
-    if (level < targetLevel - 4) {
-      let deepPixel = getInterestingPixelFromDepths(depthsCollation, params, threshold);
-      x = deepPixel.x;
-      y = deepPixel.y;
-      level += 4;
-    } else if (level < targetLevel) {
-      let ratioOfMandelbrotPoints = depthsCollation[null].length / (width * height);
-      if (ratioOfMandelbrotPoints > 0.1 || ratioOfMandelbrotPoints === 0) {
-        level -=2;
-        continue;
-      }
-      let deepPixel = getInterestingMandelbrotPoint(depthsCollation, params);
-      x = deepPixel.x;
-      y = deepPixel.y;
-      level += 1;
-    } else {
-      let deepPixel = getInterestingMandelbrotPoint(depthsCollation, params);
-      x = deepPixel.x;
-      y = deepPixel.y;
-      return {x, y, level, depth};
+    // Stop if it's too grainy
+    if (lotsOfGrainyAreas(profile)) {
+      console.log('Too much noise - adding depth adjust');
+      depthAdjust += 100;
+      level--;
+      continue;
     }
-    redraws = 0;
+
+    // Stop if there aren't clear areas 
+    if (!lotsOfNotMandlebrotAreas(profile)) {
+      console.log('Not enough clear space');
+      level--;
+      continue;
+    }
+
+    // Find which sides of the image have Mandelbrot sets
+    let sides = getMandelbrotSides(set);
+    console.log(sides);
+    if (sides.length > 2) {
+      console.log('Totally surrounded');
+      level--;
+      continue;
+    } else if (sides.length > 0) {
+      // Adjust focus point to keep the Mandelbrot in the centre.
+      console.log('Adjusting focus');
+      let pixelX = width / 2;
+      let pixelY = height / 2;
+
+      if (sides.indexOf('N') !== -1) pixelY = 0;
+      if (sides.indexOf('S') !== -1) pixelY = height;
+      if (sides.indexOf('E') !== -1) pixelX = width;
+      if (sides.indexOf('W') !== -1) pixelX = 0;
+
+      let newPoint = toComplexCoords({pixelX, pixelY}, {x, y, width, height, scale});
+      x = newPoint.x;
+      y = newPoint.y;
+
+      // Do not zoom out
+      continue;
+    }
+
+    // If there is a solid area of Mandelbrot points, then this is an
+    // acceptable area to use. It might be improved upon, though.
+    if (getMaxProportion(profile) > 0.8) {
+      potentials.push({x, y, level, depth});
+    }
+
+    // Focus on the densest proportion of Mandelbrot points.
+    let focusArea = getMaxProportionArea(profile);
+    let newPixel = {pixelX: focusArea.x * width, pixelY: focusArea.y * height};
+    let newPoint = toComplexCoords(newPixel, {x, y, width, height, scale});
+    x = newPoint.x;
+    y = newPoint.y;
+
+    // Zoom in some more if there's not enough Mandelbrot points.
+    let proportion = totalProportion(profile);
+    console.log('proportion: ', proportion);
+    if (proportion < 0.05) {
+      level++;
+      tries++; // An extra try, to get the right zoom.
+    }
   }
-  return {x, y, level: level-2, depth};
+
+  if (potentials.length) return potentials[potentials.length - 1];
 }
 
 rimrafSync('./frames/*');
 const width = 450;
 const height = 300;
 
-let target = findInterestingPoint({width: width / 3, height: height / 3});
+let target;
 
-if (target) {
-  let params = _.extend({}, target, {
-    scale: Math.pow(2, -8 - target.level),
-    width,
-    height,
-  });
-  let set = constructSet(params);
-  render(set, params, `./${md5(Date.now()).substr(0, 12)}.gif`);
-} else {
-  console.log('No target...')
+while (!target) {
+  target = scry({width: width / 3, height: height / 3});
 }
+
+let params = _.extend({}, target, {
+  scale: Math.pow(2, -8 - target.level),
+  width,
+  height,
+});
+let set = constructSet(params);
+render(set, params, `./_${md5(Date.now()).substr(0, 12)}.gif`);
