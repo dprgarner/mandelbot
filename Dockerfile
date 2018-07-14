3ab90cff5aeb82a189d21cc17121865d21a8a4b6
David Garner <dprgarner@gmail.com>
2018-07-14 19:29:36 +0100

    First bash at ffmpeg Dockerfiles

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,14 @@ FROM ${base_image}
 RUN mkdir /app
 WORKDIR /app
 
+# Compiling takes a very very long time on a Raspberry Pi. Let's hope the image works.
+COPY --from=ffmpeg /opt/ffmpeg /opt/ffmpeg/
+
+# Set up Gifsicle
+RUN apt-get update && apt-get -y install \
+    gifsicle \
+&& rm -rf /var/lib/apt/lists/*
+
 COPY package.json yarn.lock /app/
 RUN yarn
 COPY . /app/
--- build_ffmpeg_arm.sh
+++ build_ffmpeg_arm.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -e
 
 # http://www.jeffreythompson.org/blog/2014/11/13/installing-ffmpeg-for-raspberry-pi/
+# For non-pi: https://hub.docker.com/r/jrottenberg/ffmpeg/~/dockerfile/
 
-apt-get install -y --force-yes git checkinstall autoconf automake build-essential libass-dev libfreetype6-dev   libtheora-dev libtool libvorbis-dev pkg-config texinfo zlib1g-dev libv4l-dev wget
+apt-get install -y --force-yes git checkinstall autoconf automake build-essential libass-dev libfreetype6-dev libtheora-dev libtool libvorbis-dev pkg-config texinfo zlib1g-dev libv4l-dev wget
 
 cd /usr/src
 git clone git://git.videolan.org/x264
 cd x264
-./configure --host=arm-unknown-linux-gnueabi --enable-static --disable-opencl
+./configure --host=arm-unknown-linux-gnueabi --enable-static --disable-opencl --prefix=/opt/ffmpeg
 make
-sudo make -j4 install
+make -j4 install
 
-cd /usr/src
-git clone https://github.com/FFmpeg/FFmpeg.git
-cd FFmpeg
-sudo ./configure --arch=armel --target-os=linux --enable-gpl --enable-libx264 --enable-nonfree
+git clone https://github.com/FFmpeg/FFmpeg.git /usr/src/ffmpeg
+cd /usr/src/ffmpeg
+./configure --arch=armel --target-os=linux --enable-gpl --enable-libx264 --enable-nonfree --prefix=/opt/ffmpeg
 make
-sudo make -j4 install
+make -j4 install
