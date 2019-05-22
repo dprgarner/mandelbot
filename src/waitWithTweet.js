9d10047061df4b131914aa5e927b923448bca83a
David Garner <dprgarner@gmail.com>
2019-05-22 23:26:01 +0100

    Decrease tweet frequency.

--- src/waitWithTweet.js
+++ src/waitWithTweet.js
@@ -6,8 +6,8 @@ module.exports = function() {
   return Promise.resolve(true)
   .then(() => (Math.random() < 0.25) ? pinchColourScheme() : true)
   .then(() => new Promise((resolve, reject) => {
-    // Wait until the next three-hour point.
-    let tweetInterval = 1000 * 60 * 60 * 2;
+    // Wait until the next twelve-hour point.
+    let tweetInterval = 1000 * 60 * 60 * 12;
     let msUntilTime = tweetInterval - (Date.now() % tweetInterval);
     setTimeout(resolve, INSTANT ? 0 : msUntilTime);
   }));
